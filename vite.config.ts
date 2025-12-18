006574ba84a1324218c184bcff8508130d161ea7
Ali Moeeny <alimoeeny@gmail.com>
2025-12-18 14:33:24 -0500

    feat: Add CNAME for custom domain and update Vite base path to root.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pingulator'
+  base: '/' // Using custom domain, so serve from root
 })
